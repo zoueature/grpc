82510e6e82345962c2e13ad75ae09ba9df2e1959
zoueature <zoueature@gmail.com>
2024-03-09 11:36:35 +0800

    support timeout

--- client.go
+++ client.go
@@ -1,16 +1,27 @@
 package grpc
 
 import (
+	"context"
 	"errors"
 	"github.com/zoueature/config"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"time"
 )
 
 func NewClient(conf config.RpcConf) (*grpc.ClientConn, error) {
 	if conf.Type != config.RpcTypeGrpc {
 		return nil, errors.New("Rpc type not match. ")
 	}
-	conn, err := grpc.Dial(conf.CallHost(), grpc.WithInsecure(), grpc.WithBlock())
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer func() { cancel() }()
+	conn, err := grpc.DialContext(
+		ctx,
+		conf.CallHost(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+		grpc.WithIdleTimeout(10*time.Minute),
+	)
 	if err != nil {
 		return nil, err
 	}
